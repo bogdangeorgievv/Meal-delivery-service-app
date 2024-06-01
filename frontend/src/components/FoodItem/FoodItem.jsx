e6d96af61b3ea043dde9cda82c8537ff68deedee
Bogdan Georgiev <bobogeorgievv97@gmail.com>
2024-06-01 11:13:30 +0300

    update styling on Cart page

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -7,7 +7,7 @@ const FoodItem = ({id, name, price, description, image}) => {
 
   const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
 
-  return (
+  return ( 
     <div className='food-item'>
         <div className="food-item-img-container">
             <img className="food-item-image" src={image} alt="" />
